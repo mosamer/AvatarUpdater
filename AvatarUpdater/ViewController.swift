e06876e11dc88c55c2e3dbbcf89882c074ae4d46
Mostafa Amer <mostafa.amer@sociomantic.com>
2017-10-24 15:13:22 +0200

    Initial Commit

---
+++ AvatarUpdater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AvatarUpdater
+//
+//  Created by Mostafa Amer on 24.10.17.
+//  Copyright © 2017 Mostafa Amer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
