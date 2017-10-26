6d3f41dd314fe3538e4b6ef9f8932e8a09e3bd3e
mosamer <mostafamamer@gmail.com>
2017-10-26 11:43:04 +0200

    compress picked avatar
    - Implemented a dump compression technique to avoid attempt uploading large files
    - Attempt compressing until quality falls to min threshold
    - max size is ca 45KB

--- AvatarUpdater/API/UpdateAvatarEndpoint.swift
+++ AvatarUpdater/API/UpdateAvatarEndpoint.swift
@@ -12,12 +12,18 @@ struct UpdateAvatarEndpoint: Endpoint {
     let userId: String
     let avatar: UIImage
     private var encodedAvatar: String {
-        guard let data = UIImageJPEGRepresentation(avatar, 1.0) else { return "" }
+        let maxSize = 45 * 1024 /* this value is based on trial-and-error, 1MB keep failing */
+        var data = Data(count: maxSize + 1)
+        var quality: CGFloat = 1.0
+        while data.count > maxSize && quality > 0.0001 {
+            data = UIImageJPEGRepresentation(avatar, quality) ?? Data()
+            quality *= 0.7
+        }
         return data.base64EncodedString()
     }
     var path: String { return "user/\(userId)/avatar" }
     var method: HTTPMethod { return .POST}
-    var parameters: [String : Any]? { return ["avatar": encodedAvatar]}
+    var parameters: [String : Any]? { return ["avatar": encodedAvatar] }
 
     func parse(_ data: Data) throws -> URL {
         let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: String]
