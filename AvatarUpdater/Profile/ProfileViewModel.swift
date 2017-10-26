e68c9abf4bedad1c662c24567dcdf31663f44167
mosamer <mostafamamer@gmail.com>
2017-10-26 12:51:17 +0200

    initial implementation of image processing chain

--- AvatarUpdater/Profile/ProfileViewModel.swift
+++ AvatarUpdater/Profile/ProfileViewModel.swift
@@ -57,16 +57,74 @@ class ProfileViewModel: ProfileViewModelType {
             }
             .asDriver(onErrorJustReturn: #imageLiteral(resourceName: "default_avatar"))
 
-        let picked = _pickedImage.asDriver(onErrorJustReturn: #imageLiteral(resourceName: "default_avatar"))
+        let picked = _pickedImage
+            .asDriver(onErrorJustReturn: #imageLiteral(resourceName: "default_avatar"))
         return Driver.merge([fetched, picked]).distinctUntilChanged()
     }
     var userEmail: Driver<String> {
         return Driver.just(user).map { $0.email }
     }
     private let _pickedImage = PublishSubject<UIImage>()
-    var pickedImage: AnyObserver<UIImage> { return _pickedImage.asObserver()}
+    var pickedImage: AnyObserver<UIImage> {
+        // Chain order is backward as it map observers
+        return _pickedImage.asObserver()
+            .applyMonchromeFilter()
+            .resize(to: CGSize(width: 240.0, height: 240.0))
+            .crop()
+            .adjustCentering()
+            .detectFace()
+        // picked image will get into detectFace() and move upward to _pickedImage ;)
+    }
 
     var isLoading: Driver<Bool> {
         return uploadAction.executing.asDriver(onErrorJustReturn: false)
+    }
+}
+typealias ImageCropInfo = (original: UIImage, rect: CGRect)
+extension ObserverType where E == UIImage {
+    /// Apply monochrome filter to the given image
+    ///
+    /// - Returns: Filtered image
+    func applyMonchromeFilter() -> AnyObserver<UIImage> {
+        return self.mapObserver {(resizedImage: UIImage) -> UIImage in
+            let filteredImage = resizedImage
+            return filteredImage
+        }
+    }
+
+    /// resize given image to smaller square
+    ///
+    /// - Parameter newSize: square size to resize to
+    /// - Returns: resized image
+    func resize(to newSize: CGSize) -> AnyObserver<UIImage> {
+        return mapObserver {(croppedImage: UIImage) -> UIImage in
+            let resizedImage = croppedImage
+            return resizedImage
+        }
+    }
+
+    /// Crop image
+    func crop() -> AnyObserver<ImageCropInfo> {
+        return mapObserver {(original: UIImage, cropRect: CGRect) -> UIImage in
+            let croppedImage = original
+            return croppedImage
+        }
+    }
+}
+extension ObserverType where E == ImageCropInfo {
+    /// Adjust crop rect to maintain original image aspect ratio while center around square
+    func adjustCentering() -> AnyObserver<ImageCropInfo> {
+        return mapObserver {(original: UIImage, faceRect: CGRect) -> ImageCropInfo in
+            let adjustCropRect = faceRect
+            return (original: original, rect: adjustCropRect)
+        }
+    }
+
+    /// Detect face in given image. If none, return full image
+    func detectFace() -> AnyObserver<UIImage> {
+        return mapObserver {(original: UIImage) -> ImageCropInfo in
+            let faceRect = CGRect.zero
+            return (original: original, rect: faceRect)
+        }
     }
 }
